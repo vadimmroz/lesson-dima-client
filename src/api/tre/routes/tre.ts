4df4c21dc285fdec071aba45d8e0b51a91aec1a9
vadim zalozniy <vadimmroz@gmail.com>
2025-06-11 20:17:50 +0300

    123

--- src/api/tre/routes/tre.ts
+++
@@ -1,7 +0,0 @@
-/**
- * tre router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::tre.tre');
--- src/api/tre/services/tre.ts
+++
@@ -1,7 +0,0 @@
-/**
- * tre service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::tre.tre');
